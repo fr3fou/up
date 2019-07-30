2c8836349a5350b6de67ba9a61f59f3337a121f4
Simo Aleksandrov <simo3003@me.com>
2019-07-30 20:29:15 +0300

    make basic functionallity lmao

--- upload.go
+++ upload.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"math/rand"
+	"os"
 	"time"
 )
 
 // UploadFile takes in an array of bytes and lifetime in seconds and stores it
 // to the fs, returning its unique name and any errors
 func UploadFile(file []byte, lifetime time.Duration) (string, error) {
+	f, err := os.Create(generateFileName(10))
+
+	if err != nil {
+		return "", err
+	}
+
+	f.Write(file)
+	f.Close()
 
 	return "", nil
 }
